fd79abb7f64c068321c3f3591fe3867632cf99e2
javamahendra <javabymahendra@gmail.com>
2022-09-11 12:01:22 +1200

    initial commit

--- src/main/java/com/app/controller/MyController.java
+++ src/main/java/com/app/controller/MyController.java
@@ -9,7 +9,7 @@ public class MyController {
 	
 	@GetMapping(value = "/hello")
 	public String hello(@RequestParam(name = "name", defaultValue = "Mahi", required = false) String name) {
-		
+		System.out.println("Hello...");
 		return "Hello Jenkins CI SpringMVCFormApp Executing... ::"+name;
 	}
 
